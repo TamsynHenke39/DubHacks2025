6d06e3278cd4cb556b6a4d39a22ba5b9f51b23f9
Tamsyn Henke <tamsynhenke@gmail.com>
2025-10-19 10:13:20 -0700

    removed the cursing from the payment.tsx lmao

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -2,7 +2,7 @@ function Payment () {
 
     return (
         <>
-        Ryan dev page ryan dev page don't FUCK up the code base
+        Ryan dev page ryan dev page don't mess up the code base
         </>
     )
 
